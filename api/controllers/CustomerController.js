e77a3fc7db3f47034f71d6615eb68213959334b1
codetv <codetvofficial@gmail.com>
2016-04-23 21:07:27 +0200

    Customer Model Added

---
+++ api/controllers/CustomerController.js
@@ -0,0 +1,11 @@
+/**
+ * CustomerController
+ *
+ * @description :: Server-side logic for managing customers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
--- config/connections.js
+++ config/connections.js
@@ -40,13 +40,13 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMysqlServer: {
-  //   adapter: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER', //optional
-  //   password: 'YOUR_MYSQL_PASSWORD', //optional
-  //   database: 'YOUR_MYSQL_DB' //optional
-  // },
+   sailsmysql: {
+     adapter: 'sails-mysql',
+     host: 'localhost',
+     user: 'root', //optional
+     password: '', //optional
+     database: 'demo' //optional
+   },
 
   /***************************************************************************
   *                                                                          *
