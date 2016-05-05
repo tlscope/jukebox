fcc8e5df8cae47c12373484fc51c19ec20bba807
codetv <codetvofficial@gmail.com>
2016-05-05 17:03:41 +0200

    Views Added

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -6,6 +6,22 @@
  */
 
 module.exports = {
-	
+
+	add:function(req,res){
+		res.view();
+	},
+	create:function(req,res){
+		Customer.create(req.params.all(),function(err,customer){
+			res.redirect('/customer/all');
+		});
+	},
+	all:function(req,res){
+		Customer.find(function(err,customers){
+			res.view({
+				customers:customers
+			});
+		});
+	}
+
 };
 
